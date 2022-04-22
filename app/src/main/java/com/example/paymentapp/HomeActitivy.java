8f8258e1ee1f1cbfa5f63c8a6e9aaa863b4a6024
Kortlon Erskine <kortlonerskine@me.com>
2022-04-21 22:19:59 -0500

    Working nav tabs

--- app/src/main/java/com/example/paymentapp/HomeActitivy.java
+++ app/src/main/java/com/example/paymentapp/HomeActitivy.java
@@ -1,13 +1,12 @@
 package com.example.paymentapp;
 
-import androidx.appcompat.app.ActionBar;
+import android.os.Bundle;
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.os.Bundle;
 
 public class HomeActitivy extends AppCompatActivity {
     private DrawerLayout drawer;
@@ -17,14 +16,16 @@ public class HomeActitivy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_actitivy);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        drawer = findViewById(R.id.drawer_layout);
+         Toolbar toolbar = findViewById(R.id.toolbar);
+      //  Toolbar toolbar = findViewById(R.id.toolbar);
+         setSupportActionBar(toolbar);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+            drawer = findViewById(R.id.drawer_layout);
+
+           ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+             drawer.addDrawerListener(toggle);
+            toggle.syncState();
     }
     @Override
     public void onBackPressed(){
--- app/src/main/java/com/example/paymentapp/LoginAct.java
+++ app/src/main/java/com/example/paymentapp/LoginAct.java
@@ -33,7 +33,6 @@ public class LoginAct  extends AppCompatActivity {
                     Boolean checkuserpass = DB.checkuserpass(user,pass);
                     if(checkuserpass ==true){
                         Toast.makeText(LoginAct.this,"Sign in successfull", Toast.LENGTH_SHORT).show();
-                        
                         Intent intent = new Intent(getApplicationContext(), HomeActitivy.class);
                         startActivity(intent);
 
