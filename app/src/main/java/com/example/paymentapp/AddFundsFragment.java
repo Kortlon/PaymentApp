bde50ef776c1e7485e03609d0b42128d7f3a6976
kortlon <kortlonerskine@me.com>
2022-04-26 21:18:00 -0500

    work in progress

--- app/src/main/java/com/example/paymentapp/AddFundsFragment.java
+++ app/src/main/java/com/example/paymentapp/AddFundsFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.content.Intent;
 
@@ -11,8 +15,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import java.util.List;
+
 public class AddFundsFragment extends Fragment {
     DBHelper DB;
+    Button addFundButton;
+    EditText addAmount;
+    Spinner chooseAccount;
     @Nullable
     @Override
 
@@ -26,5 +35,24 @@ public class AddFundsFragment extends Fragment {
 
     }
 
+   /* private void loadAccountSpinner(){
+
+        List<String> accounts = DB.getAccounts();
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, accounts);
+        dataAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        chooseAccount.setAdapter(dataAdapter);
+    }
+    private void addFundsTransaction(){
+        addFundButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String value= addAmount.getText().toString();
+                int finalValue=Integer.parseInt(value);
+                DB.insertDataTransaction(finalValue, "Added Funds");
+            }
+        });
+    }
+*/
 
 }
--- app/src/main/java/com/example/paymentapp/DBHelper.java
+++ app/src/main/java/com/example/paymentapp/DBHelper.java
@@ -8,9 +8,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final String DBNAME = "Login.db";
+    public static final String amountCol = "amount";
+    public static final String transactionLabel = "label";
 
     public DBHelper(@Nullable Context context) {
         super(context, "Login.db", null , 1) ;
@@ -19,10 +24,12 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase MyDB) {
     MyDB.execSQL("create Table users(username TEXT primary key, password TEXT)");
-    MyDB.execSQL("create Table cards(username TEXT primary key, cardnum TEXT)");
+    MyDB.execSQL("create Table cards(username TEXT primary key, cardnum TEXT, bal REAL)");
     MyDB.execSQL("create Table linkaccounts(AccountNum INT primary key, RoutingNUM INT, username TEXT)");
     MyDB.execSQL("create Table userdata(username TEXT primary key, email TEXT, phonenum TEXT, FName TEXT, LNAME TEXT )");
     MyDB.execSQL("create Table balance(cardnum TEXT primary key, bal REAL)");
+    MyDB.execSQL("create Table transactions(id INTEGER primary key, amount REAL, label TEXT)");
+
     }
 
     @Override
@@ -99,5 +106,33 @@ public class DBHelper extends SQLiteOpenHelper {
         else
             return false;
 
+    }
+
+    public List<String> getAccounts(){
+        List<String> accounts = new ArrayList<String>();
+        String selectQuery = "select AccountNum from linkaccounts";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        if (cursor.moveToFirst()) {
+            do {
+                accounts.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return accounts;
+    }
+
+    public boolean insertDataTransaction(int amount, String label){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(amountCol, amount);
+        contentValues.put(transactionLabel, label);
+        db.insert("transactions", null, contentValues);
+        long result = db.insert("transactions",null, contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
     }
 }
